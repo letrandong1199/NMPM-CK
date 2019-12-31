51c3900e82a0914114693763723afb3c4bf89e87
nguyentruongtoan <toannguyentruong1309@gmail.com>
2019-12-31 17:23:58 +0700

    create a listview tour, search frame add button in main activity

--- ViTour/app/src/main/java/com/nmpm/vitour/MainActivity.java
+++ ViTour/app/src/main/java/com/nmpm/vitour/MainActivity.java
@@ -1,17 +1,22 @@
 package com.nmpm.vitour;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_location);
-
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
     }
 }
