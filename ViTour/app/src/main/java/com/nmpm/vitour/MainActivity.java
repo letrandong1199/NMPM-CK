a807df2470c2f7f49acefd3a4e464763db8dbb49
unknown <letrandong1199@gmail.com>
2019-12-30 20:41:11 -0800

    create layout location and trip

---
+++ ViTour/app/src/main/java/com/nmpm/vitour/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nmpm.vitour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_location);
+
+    }
+}
