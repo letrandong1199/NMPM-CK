d9259e3d84f413b379e33366fb338bb4936966b3
nguyentruongtoan <toannguyentruong1309@gmail.com>
2020-01-05 14:36:25 +0700

    Revert "Tạo các layout edit profile, forgot pasword, stop point, detail tour"
    This reverts commit cf701ba4ad4316532b3483926bfdba5f1a9fe939.

--- ViTour/app/src/main/java/com/nmpm/vitour/SignUpActivity.java
+++ ViTour/app/src/main/java/com/nmpm/vitour/SignUpActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.sign_up_layout);
     }
 }
